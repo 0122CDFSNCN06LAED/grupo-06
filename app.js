0e392b69f578c712d8ee88d11584264e0c16d5ad
Alec Coleman <acoleman85@gmail.com>
2022-03-20 16:53:25 -0300

    pagina login

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-    console.log("Servidor corriendo en el puerto 3000");
-  });
+  console.log("Servidor corriendo en el puerto 3000");
+});
 
 app.get("/", function (req, res) {
-//    console.log(__dirname);
-    let htmlPath = path.resolve(__dirname, "./views/index.html");
-    res.sendFile(htmlPath);
+  //           console.log(__dirname);
+  let htmlPath = path.resolve(__dirname, "./views/index.html");
+  res.sendFile(htmlPath);
+});
+
+app.get("/login", function (req, res) {
+  //           console.log(__dirname);
+  let htmlPath = path.resolve(__dirname, "./views/login.html");
+  res.sendFile(htmlPath);
 });
