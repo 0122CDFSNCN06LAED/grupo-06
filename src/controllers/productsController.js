6652d8e19842af337fa78f848763ada2ebeb0761
aleccoleman-glb <96244347+aleccoleman-glb@users.noreply.github.com>
2022-04-18 17:37:37 -0300

    ajuste archivos carga formulario Isa

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,6 +5,10 @@ const productsController = {
     let htmlPath = path.resolve("./src/views/products/productDetail.ejs");
     res.render(htmlPath);
   },
+  add: function(req,res){
+    let htmlPath = path.resolve("./src/views/products/addProduct.ejs")
+    res.render(htmlPath);
+  }
 };
 
 module.exports = productsController;
