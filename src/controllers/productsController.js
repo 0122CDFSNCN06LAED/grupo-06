a61f4fcc9ad5897aae80eab2d93d39644d4939dd
JosiPaolini <joseignaciopaolini@gmail.com>
2022-04-27 18:57:42 -0300

    Cargar productos método "store"

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const { text } = require("express");
 
 const productsFilePath = path.join(__dirname, "../database/products.json");
 
@@ -27,33 +28,49 @@ const productsController = {
     res.render(htmlPath);
   },
   store: (req, res) => {
-    res.send(req.body);
+    //res.send(req.body);
+
+    const lastIndex = helpers.length - 1;
+    const lastProduct = helpers[lastIndex];
+    const biggestId = lastProduct ? lastProduct.id : 0;
+    const newId = biggestId + 1;
+
+    const newProduct = {
+      ...req.body,
+      id: newId,
+    };
+
+    helpers.push(newProduct);
+
+    const jsonTxt = JSON.stringify(helpers, null, 2);
+    fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
+
+    res.redirect("/products");
   },
 
   edit: function (req, res) {
     const id = req.params.id;
-    
+
     const helper = helpers.find((p) => id == p.id);
-    
+
     let htmlPath = path.resolve("./src/views/products/editHelper.ejs");
     res.render(htmlPath, {
       helper,
     });
   },
-  update: function (req,res){
+  update: function (req, res) {
     const id = req.params.id;
-    
+
     const helper = helpers.find((p) => id == p.id);
 
     Object.assign(helper, {
-       ...req.body
-     });
+      ...req.body,
+    });
 
     const jsonTxt = JSON.stringify(helpers, null, 2);
     fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
 
     res.redirect("/products/");
-
   },
 
   erase: function (req, res) {
