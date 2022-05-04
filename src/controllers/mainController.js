e4db0ccafeffa2ffa2ff8f131b0689f4240cb880
lauo2211@hotmail.com <lauo2211@hotmail.com>
2022-05-04 18:56:25 -0500

    actualizacion del home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,20 @@
-const path = require("path");
+
+const path = require("path")
+const productsFilePath = path.join(__dirname, "../database/products.json");
+
+
+const helpers = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
 
 const mainController = {
   index: function (req, res) {
-    
+    let helpermasbuscados = helpers.filter((h)=> h.masbuscados == true)
     let htmlPath = path.resolve("./src/views/main/index.ejs");
-    res.render(htmlPath);
+    res.render(htmlPath
+      , {
+        helpermasbuscados:helpermasbuscados
+      });
+
   },
   checkout: function (req,res){
       let htmlPath = path.resolve("./src/views/main/check-out.ejs");
