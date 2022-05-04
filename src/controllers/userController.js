203a4f5f97058b6fc71f204073480b4f295dd6fa
JosiPaolini <joseignaciopaolini@gmail.com>
2022-05-03 21:50:08 -0300

    cargar productos. Método "store"

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,10 @@
 const path = require("path");
+const fs = require("fs");
+const { text } = require("express");
+
+const userFilePath = path.join(__dirname, "../database/users.json");
+
+const user = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
 
 const userController = {
   login: function (req, res) {
@@ -9,6 +15,33 @@ const userController = {
     let htmlPath = path.resolve("./src/views/user/registerUser.ejs");
     res.render(htmlPath);
   },
+
+  //addUser: function (req, res) {
+  //  let htmlPath = path.resolve("./src/views/user/registerUser.ejs");
+  //  res.render(htmlPath);
+  //},
+
+  storeUser: (req, res) => {
+    //res.send(req.body);
+
+    const lastIndex = user.length - 1;
+    const lastUser = user[lastIndex];
+    const biggestId = lastUser ? lastUser.id : 0;
+    const newUserID = biggestId + 1;
+
+    const newUser = {
+      ...req.body,
+      id: newUserID,
+    };
+
+    user.push(newUser);
+
+    const jsonTxt = JSON.stringify(user, null, 2);
+    fs.writeFileSync(userFilePath, jsonTxt, "utf-8");
+
+    res.redirect("/user/login");
+  },
+
   registerUserOrHelper: function (req, res) {
     let htmlPath = path.resolve("./src/views/user/registerUserOrHelper.ejs");
     res.render(htmlPath);
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,7 @@ const userController = require("../controllers/userController.js");
 
 router.get("/login", userController.login);
 router.get("/registerUser", userController.register);
-router.get("/registerUserOrHelper", userController.registerUserOrHelper);
-
+//router.get("/registerUserOrHelper", userController.registerUserOrHelper);
+//router.get("/registerUser", userController.addUser);
+router.post("/storeUser", userController.storeUser);
 module.exports = router;
