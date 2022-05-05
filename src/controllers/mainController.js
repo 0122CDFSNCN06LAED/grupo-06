b4cb771be2d4382c0b10c7530ce16234a7184a58
Alec Coleman <acoleman85@gmail.com>
2022-05-04 21:00:04 -0300

    fix bug require fs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,25 +1,22 @@
+const fs = require("fs");
 
-const path = require("path")
+const path = require("path");
 const productsFilePath = path.join(__dirname, "../database/products.json");
-
 
 const helpers = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
-
 const mainController = {
   index: function (req, res) {
-    let helpermasbuscados = helpers.filter((h)=> h.masbuscados == true)
+    let helpermasbuscados = helpers.filter((h) => h.masbuscados == true);
     let htmlPath = path.resolve("./src/views/main/index.ejs");
-    res.render(htmlPath
-      , {
-        helpermasbuscados:helpermasbuscados
-      });
-
+    res.render(htmlPath, {
+      helpermasbuscados: helpermasbuscados,
+    });
   },
-  checkout: function (req,res){
-      let htmlPath = path.resolve("./src/views/main/check-out.ejs");
-      res.render(htmlPath);
-  }
+  checkout: function (req, res) {
+    let htmlPath = path.resolve("./src/views/main/check-out.ejs");
+    res.render(htmlPath);
+  },
 };
 
 module.exports = mainController;
