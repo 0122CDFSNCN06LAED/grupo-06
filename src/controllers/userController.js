400a59983d615448a9029c953bf4891470d54d39
JosiPaolini <joseignaciopaolini@gmail.com>
2022-05-30 20:01:23 -0300

    validator preliminar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const bcrypt = require("bcryptjs");
-const { text } = require("express");
+//const { text } = require("express");
 const { validationResult } = require("express-validator");
 
 const userFilePath = path.join(__dirname, "../database/users.json");
@@ -11,20 +11,19 @@ const user = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
 const userController = {
   login: function (req, res) {
     let htmlPath = path.resolve("./src/views/user/login.ejs");
-    res.render(htmlPath, {user: req.session.usuariologueado});
+    res.render(htmlPath, { user: req.session.usuariologueado });
   },
   processLogin: function (req, res) {
     let error = validationResult(req);
-    if (error.isEmpty()) 
-    {
+    if (error.isEmpty()) {
       const userFilePath = path.join(__dirname, "../database/users.json");
       const user = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
       let usuarioAlloguearse = undefined;
-      
+
       for (let i = 0; i < user.length; i++) {
         if (user[i].email == req.body.email) {
           if (bcrypt.compareSync(req.body.password, user[i].password)) {
-            usuarioAlloguearse = user[i].firstName + ' ' + user[i].lastName;
+            usuarioAlloguearse = user[i].firstName + " " + user[i].lastName;
             req.session.usuariologueado = usuarioAlloguearse;
             req.session.idUsuario = user[i].id;
             break;
@@ -34,19 +33,23 @@ const userController = {
 
       if (usuarioAlloguearse == undefined) {
         let htmlPath = path.resolve("./src/views/user/login.ejs");
-        res.render(htmlPath,{errors: [{
-           msg:('credenciales invalidas')
-         }],
-        user : req.session.usuariologueado});
-      }
-      else {
+        res.render(htmlPath, {
+          errors: [
+            {
+              msg: "credenciales invalidas",
+            },
+          ],
+          user: req.session.usuariologueado,
+        });
+      } else {
         res.redirect("../");
       }
-    } 
-      else 
-    {
+    } else {
       let htmlPath = path.resolve("./src/views/user/login.ejs");
-      res.render(htmlPath, { errors: error.errors, user: req.session.usuariologueado });
+      res.render(htmlPath, {
+        errors: error.errors,
+        user: req.session.usuariologueado,
+      });
     }
   },
 
@@ -61,41 +64,46 @@ const userController = {
   //},
 
   storeUser: (req, res) => {
-    //res.send(req.body);
+    const resultValidationUsers = validationResult(req);
+    let htmlPath = path.resolve("./src/views/user/registerUser.ejs");
+    if (resultValidationUsers.errors.length > 0) {
+      const lastIndex = user.length - 1;
+      const lastUser = user[lastIndex];
+      const biggestId = lastUser ? lastUser.id : 0;
+      const newUserID = biggestId + 1;
 
-    const lastIndex = user.length - 1;
-    const lastUser = user[lastIndex];
-    const biggestId = lastUser ? lastUser.id : 0;
-    const newUserID = biggestId + 1;
+      const newUser = {
+        id: newUserID,
+        firstName: req.body.Nombre,
+        lastName: req.body.Apellido,
+        phone: req.body.Telefono,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        ...req.file,
+      };
 
-    const newUser = {
-      id: newUserID,
-      firstName: req.body.Nombre,
-      lastName: req.body.Apellido,
-      phone: req.body.Telefono,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
-      ...req.file
-    };
+      user.push(newUser);
 
-    user.push(newUser);
-
-    const jsonTxt = JSON.stringify(user, null, 2);
-    fs.writeFileSync(userFilePath, jsonTxt, "utf-8");
-
-    res.redirect("/user/login");
+      const jsonTxt = JSON.stringify(user, null, 2);
+      fs.writeFileSync(userFilePath, jsonTxt, "utf-8");
+      res.redirect("/user/login");
+    } else {
+      return res.render(htmlPath, {
+        errors: resultValidationUsers.mapped(),
+      });
+    }
   },
 
-  registerUserOrHelper: function (req, res) {
-    let htmlPath = path.resolve("./src/views/user/registerUserOrHelper.ejs");
-    res.render(htmlPath, { user: req.session.usuariologueado });
-  },
+  //registerUserOrHelper: function (req, res) {
+  //  let htmlPath = path.resolve("./src/views/user/registerUserOrHelper.ejs");
+  //  res.render(htmlPath, { user: req.session.usuariologueado });
+  //},
 
-  signOut: function(req, res) {
+  signOut: function (req, res) {
     req.session.destroy(function () {
       res.redirect("/user/login");
     });
-  }
+  },
 };
 
 module.exports = userController;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const {check} = require("express-validator")
+const { check } = require("express-validator");
+const { body } = require("express-validator");
 const validations = [
   check("email").notEmpty().withMessage("Ingrese Un Usuario"),
   check("email").isEmail().withMessage("Email Invalido"),
   check("password").notEmpty().withMessage("Ingrese una Contraseña"),
 ];
+const validationRegister = [
+  body("Apellido").notEmpty().withMessage("Complete su apellido"),
+  body("Nombre").notEmpty().withMessage("Complete su nombre"),
+  body("Telefono").notEmpty().withMessage("Complete su teléfono"),
+  body("email").notEmpty().withMessage("Complete su email"),
+  body("confirm-email").notEmpty().withMessage("Vuelva a completar su email"),
+];
+
 const multer = require("multer");
-const { body } = require("express-validator");
+//const { body } = require("express-validator");
 const loginValidations = require("../validations/loginValidations.js");
 const urlencoded = require("express");
 
 const guestMiddleware = require("../middleware/guestMiddleware.js");
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -32,13 +40,13 @@ const userController = require("../controllers/userController.js");
 router.get("/login", userController.login);
 router.post("/login", validations, userController.processLogin);
 
-
 router.get("/registerUser", userController.register);
 //router.get("/registerUserOrHelper", userController.registerUserOrHelper);
 //router.get("/registerUser", userController.addUser);
 
 router.post(
   "/storeUser",
+  validationRegister,
   uploadFile.single("userImage"),
   userController.storeUser
 );
