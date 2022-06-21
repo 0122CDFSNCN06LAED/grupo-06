2b70528dfd5c9a1755534ccdebc149b88cda9431
JosiPaolini <joseignaciopaolini@gmail.com>
2022-06-21 19:11:24 -0300

    bag fix profiles

--- src/database/models/Profile.js
+++ src/database/models/Profile.js
@@ -1,35 +1,32 @@
 module.exports = (sequelize, dataTypes) => {
-    //nombre tabla
-    let alias = "Profile"
-    //columnas y tipos de datos:
-    let columnas = {
-        id: {
-            type: dataTypes.BIGINT(10).UNSIGNED,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: dataTypes.STRING(100),
-            allowNull: false
-        },
-    };
+  //nombre tabla
+  let alias = "Profile";
+  //columnas y tipos de datos:
+  let columnas = {
+    id: {
+      type: dataTypes.BIGINT(10).UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+    },
+  };
 
-    let config = {
-        tableName: "profiles",
-        timestamps: false,
+  let config = {
+    tableName: "profiles",
+    timestamps: false,
+  };
 
-    }
+  const Profile = sequelize.define(alias, columnas, config);
 
-    const Profile = sequelize.define(alias, columnas, config);
-
-    Profile.associate = function (models) {
-        //un oficio pertenece a varios helpers??
-        Profile.hasMany(models.User, {
-            as: "user",
-            foreignKey: "profile_id"
-
-        });
-
-    }
-return Profile
-}
+  Profile.associate = function (models) {
+    //un oficio pertenece a varios helpers??
+    Profile.hasMany(models.User, {
+      as: "user",
+      foreignKey: "profile_id",
+    });
+  };
+  return Profile;
+};
