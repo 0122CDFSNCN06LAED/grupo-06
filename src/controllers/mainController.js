d4fc6d16dcdbd098fcc17c08cf70acadcd8c6508
Alec Coleman <acoleman85@gmail.com>
2022-06-21 19:06:51 -0300

    ajuste home - mas buscados de la BBDD

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,16 +3,33 @@ const fs = require("fs");
 const path = require("path");
 const productsFilePath = path.join(__dirname, "../database/products.json");
 
+const db = require("../database/models");
+
 const helpers = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const mainController = {
   index: function (req, res) {
-    let helpermasbuscados = helpers.filter((h) => h.masbuscados == true);
     let htmlPath = path.resolve("./src/views/main/index.ejs");
-    res.render(htmlPath, {
-      helpermasbuscados: helpermasbuscados,
-      user: req.session.usuariologueado,
-    });
+    db.Helper.findAll({
+      where: {
+        mas_buscados: 1
+      },
+      include: ['user', 'oficio']
+    })
+      .then((helpersmasbuscados) => {
+        res.render(htmlPath, {
+          helpermasbuscados: helpersmasbuscados,
+          user: req.session.usuariologueado,
+        })
+      })
+    
+    
+    // let helpermasbuscados = helpers.filter((h) => h.masbuscados == true);
+    // let htmlPath = path.resolve("./src/views/main/index.ejs");
+    // res.render(htmlPath, {
+    //   helpermasbuscados: helpermasbuscados,
+    //   user: req.session.usuariologueado,
+    // });
   },
   checkout: function (req, res) {
     let htmlPath = path.resolve("./src/views/main/check-out.ejs");
