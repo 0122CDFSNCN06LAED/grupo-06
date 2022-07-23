f2acb2ae8e4ebd8a369c536abf1e11bd8421748a
Alec Coleman <acoleman85@gmail.com>
2022-07-23 14:14:33 -0300

    completar lista usuarios API

--- src/controllers/api/userAPIController.js
+++ src/controllers/api/userAPIController.js
@@ -3,15 +3,31 @@ const db = require("../../database/models");
 const userAPIController = {
   list: (req, res) => {
     // agregar el resto de los atributos
-    db.User.findAll({ attributes: ["id", "first_name", "last_name"] }).then(
-      (users) => {
-        let respuesta = {
-          count: users.length,
-          users,
-        };
-        res.json(respuesta);
-      }
-    );}
+    db.User.findAll({
+      attributes: [
+        "id",
+        [
+          db.User.sequelize.fn(
+            "CONCAT",
+            db.User.sequelize.col("first_name"),
+            " ",
+            db.User.sequelize.col("last_name")
+          ),
+          "full_name",
+        ],
+        "email",
+        [
+          db.User.sequelize.fn("CONCAT", "localhost:3000/api/users/",db.User.sequelize.col("id")),
+          "detail_url",
+        ],
+      ],
+    }).then((users) => {
+      let respuesta = {
+        count: users.length,
+        users,
+      };
+      res.json(respuesta);
+    });}
 //  agregar método para traer detalle
 };
 
