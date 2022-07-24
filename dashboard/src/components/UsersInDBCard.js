35cea199b37a64c84698ac9d4f193c8f49c324f5
Alec Coleman <acoleman85@gmail.com>
2022-07-24 19:41:08 -0300

    push primer react

---
+++ dashboard/src/components/UsersInDBCard.js
@@ -0,0 +1,10 @@
+import BigCard from "./BigCard";
+import UsersInDb from "./UsersInDb";
+
+export default function UsersInDbCard() {
+  return (
+    <BigCard title="Genres in database">
+      <UsersInDb />
+    </BigCard>
+  );
+}
