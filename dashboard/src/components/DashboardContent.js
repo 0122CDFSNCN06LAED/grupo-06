cd4ae1458832a3e209671cf27f8d5a3e4a06c331
aleccoleman-glb <96244347+aleccoleman-glb@users.noreply.github.com>
2022-07-26 10:26:19 -0300

    ajuste cabecera dashboard

--- dashboard/src/components/DashboardContent.js
+++ dashboard/src/components/DashboardContent.js
@@ -5,7 +5,7 @@ function DashboardContent() {
   return (
     <>
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">Dashboard de Operaciones</h1>
+        <h1 className="h2 mb-0 text-primary">Dashboard de Operaciones</h1>
       </div>
       <SmallCardRow />
       <BigCardRow />
