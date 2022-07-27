2004a51f7ebc00d0f1cd7824b9234e638c8017d7
aleccoleman-glb <96244347+aleccoleman-glb@users.noreply.github.com>
2022-07-27 09:17:42 -0300

    limpieza archivos no necesarios

--- dashboard/src/components/BigCardRow.js
+++ dashboard/src/components/BigCardRow.js
@@ -1,5 +1,4 @@
 import UsersInDbCard from "./UsersInDBCard";
-import LastMovieInDb from "./LastMovieCard";
 import OficiosInDbCard from "./OficiosInDBCard";
 import HelpersInDbCard from "./HelpersInDBCard";
 import LastHelperCard from "./LastHelperCard"
@@ -7,8 +6,6 @@ import LastHelperCard from "./LastHelperCard"
 function BigCardRow() {
   return (
     <div className="row">
-      {/* <!-- Last Movie in DB --> */}
-      {/* <LastMovieInDb /> */}
       <UsersInDbCard />
       {/* <HelpersInDbCard /> */}
       <LastHelperCard />
