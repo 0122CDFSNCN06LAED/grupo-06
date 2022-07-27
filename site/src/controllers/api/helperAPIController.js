6c1dbcfb9374c6566f2eefc6e39d1a7dd9f63b4a
Alec Coleman <acoleman85@gmail.com>
2022-07-27 00:00:32 -0300

    ajuste API helper list

--- site/src/controllers/api/helperAPIController.js
+++ site/src/controllers/api/helperAPIController.js
@@ -1,44 +1,113 @@
 
 const db = require("../../database/models");
 const helperAPIController = {
-  list: (req, res) => {
+  list: (req,res) => {
     db.Helper.findAll({
-      include: ["user"],
-    }).then((helper) => {
-      // db.User.findAll({
-      //   attributes: [
-      //     "id",
-      //     [
-      //       db.User.sequelize.fn(
-      //         "CONCAT",
-      //         db.User.sequelize.col("first_name"),
-      //         " ",
-      //         db.User.sequelize.col("last_name")
-      //       ),
-      //       "full_name",
-      //     ],
-      //     "email",
-      //     [
-      //       db.User.sequelize.fn(
-      //         "CONCAT",
-      //         "localhost:3000/api/helper/",
-      //         db.User.sequelize.col("id")
-      //       ),
-      //       "detail_url",
-      //     ],
-      //   ],
-      // }).then((user) => {
-        let respuesta = {
-          count: helper.length,
-
-          helper,
-          //user,
-        };
-        res.json(respuesta);
-      });
-    }
-    //);
-  };
-//};
+      group: "oficio_id",
+      raw: true,
+      include: [
+        {
+          model: db.Oficio,
+          attributes: [],
+          as: "oficio",
+        },
+      ],
+      attributes: [
+        [db.Helper.sequelize.col("oficio.name"), "nombre_oficio"],
+        [
+          db.Helper.sequelize.fn("COUNT", db.Helper.sequelize.col("helper.id")),
+          "countOficios",
+        ],
+      ],
+    })
+      .then((groupOficios) => {
+        db.Helper.findAll({
+          include: [
+            {
+              model: db.User,
+              attributes: [],
+              as: "user",
+            },
+            {
+              model: db.Oficio,
+              attributes: [],
+              as: "oficio",
+            },
+          ],
+          attributes: [
+            [db.Helper.sequelize.col("helper.id"), "Id"],
+            [
+              db.Helper.sequelize.fn(
+                "CONCAT",
+                db.Helper.sequelize.col("user.first_name"),
+                " ",
+                db.Helper.sequelize.col("user.last_name")
+              ),
+              "full_name",
+            ],
+            [db.Helper.sequelize.col("oficio.name"), "nombre_oficio"],
+            "descripcion",
+            "anos_de_experiencia",
+            "tarifa",
+            [
+              db.Helper.sequelize.fn(
+                "CONCAT",
+                "localhost:3000/products/detail/",
+                db.Helper.sequelize.col("helper.id")
+              ),
+              "detail_url",
+            ],
+          ],
+        }).then((helper) => {
+          const respuesta = {
+            count: helper.length,
+            group: groupOficios,
+            helper,
+          };
+          res.json(respuesta);
+        });
+}  )
+  }
+  
+  // list: (req, res) => {
+  //   db.Helper.findAll({
+  //     group: "oficio_id",
+  //     raw: true,
+  //     attributes: {
+  //       include: [
+  //         [
+  //           db.Helper.sequelize.fn(
+  //             "COUNT",
+  //             db.Helper.sequelize.col("helper.id")
+  //           ),
+  //           "countOficios",
+  //         ],
+  //         [db.Helper.sequelize.col('oficio.name'),"oficio"]
+  //       ],
+  //     },
+  //     include:[{
+  //       model: db.Oficio,
+  //       required: false,
+  //       as:'oficio',
+  //       attributes:[]
+  //     }],
+  //     includeIgnoreAttributes: false,
+  //   }).then((countHelpers) => {
+  //     db.Helper.findAll({
+  //       include: ["user", "oficio"],
+  //     }).then((helper) => {
+  //       let respuesta = {
+  //         count: helper.length,
+  //         countOficio: countHelpers,
+  //         helper,
+  //       };
+  //       res.json(respuesta);
+  //     });
+  //   });
+  //   }
+  //   //);
+  // };
+//
+};
 
 module.exports = helperAPIController;
